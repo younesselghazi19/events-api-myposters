5d959f1216796d492a11ab074d1bd9ca73397b4f
ELGHAZI YOUNESS p2101998 <youness.elghazi@etu.univ-lyon1.fr>
2022-05-26 16:24:24 +0200

    fixing db setting with Heroku

--- services/dbConfig.js
+++ services/dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(
-    "mongodb+srv://younesselghazi:159y159Y@cluster0.peu4t.mongodb.net/Events",
+    process.env.MONGO_URL,
     { useNewUrlParser: true, useUnifiedTopology: true },
     (err) => {
         if (!err) console.log("Mongodb connected");
